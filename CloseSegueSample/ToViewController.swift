3082fe44c61639479ab5e575945543b1f86d8525
TakahashiIkki <new_ittyandesu@outlook.jp>
2016-06-23 13:55:07 +0900

    make ToViewController

--- CloseSegueSample/ToViewController.swift
+++ CloseSegueSample/ToViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ToViewController.swift
 //  CloseSegueSample
 //
 //  Created by 一騎高橋 on 2016/06/23.
@@ -8,20 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ToViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func returnToMe(segue: UIStoryboardSegue) {
-    
-    }
+        
 }
-
